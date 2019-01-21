9c7b869840527169078c3ac45f10f601cf08d550
Justin Mac <jmac006@ucr.edu>
2019-01-21 08:47:38 -0800

    Added more ASINs, trying to implement data visualization next

--- Amazon Scraper.py
+++ Amazon Scraper.py
@@ -11,6 +11,7 @@ import requests
 #from exceptions import ValueError
 from time import sleep
 import re
+import time
 
 
 def AmzonParser(url, ASIN):
@@ -35,8 +36,7 @@ def AmzonParser(url, ASIN):
 			RAW_RANKING = doc.xpath(XPATH_RANKING)
 
 
-			#Convert to arry to string and clean-up string
-			#print(RAW_RANKING)
+			#Convert array to string and clean-up string
 			NAME = ' '.join(''.join(RAW_NAME).split()) if RAW_NAME else None
 			SALE_PRICE = ' '.join(''.join(RAW_SALE_PRICE).split()).strip() if RAW_SALE_PRICE else None #strip() removes whitespace
 			CATEGORY = ' > '.join([i.strip() for i in RAW_CATEGORY]) if RAW_CATEGORY else None
@@ -53,10 +53,10 @@ def AmzonParser(url, ASIN):
 				RANKING = ["No Ranking"]
 				data = {
 					'NAME':NAME,
-					'SALE_PRICE':SALE_PRICE,
+					'PRICE':SALE_PRICE,
 					'CATEGORY':CATEGORY,
-					'ORIGINAL_PRICE':ORIGINAL_PRICE,
-					'AVAILABILITY':AVAILABILITY,
+					#'ORIGINAL_PRICE':ORIGINAL_PRICE,
+					#'AVAILABILITY':AVAILABILITY,
 					'URL':url,
 					'RANK':RANKING,
 					}
@@ -66,7 +66,9 @@ def AmzonParser(url, ASIN):
 				ORIGINAL_PRICE = SALE_PRICE
 
 			if page.status_code!=200:
+				print("Captcha")
 				raise ValueError('captha')
+				return data
 
 
 			RANKING = RANKING.replace('\n','')
@@ -77,46 +79,75 @@ def AmzonParser(url, ASIN):
 			RANKING = RANKING.replace("_rank", " ")
 			RANKING = RANKING.replace("\u00a0", ' ')
 
-			#print(type(RANKING))
 			RANKING = RANKING.split("#") #convert string to list
 			for i in range(len(RANKING)):
 				RANKING[i] = RANKING[i].strip()
-			#print(type(RANKING))
-			#while()
 
 			print(RANKING)
 
 			data = {
 					'NAME':NAME,
-					'SALE_PRICE':SALE_PRICE,
+					'PRICE':SALE_PRICE,
 					'CATEGORY':CATEGORY,
-					'ORIGINAL_PRICE':ORIGINAL_PRICE,
-					'AVAILABILITY':AVAILABILITY,
+					#'ORIGINAL_PRICE':ORIGINAL_PRICE,
+					#'AVAILABILITY':AVAILABILITY,
 					'URL':url,
 					'RANK':RANKING,
 					}
  
 			return data
 		except Exception as e:
+			print("Error")
 			print(e)
+			return data
 
 def ReadASIN():
 	# ASIN_List = csv.DictReader(open(os.path.join(os.path.dirname(__file__),"Asinfeed.csv")))
-	ASIN_List = ['B06Y27GL9S', 
-				'B0089175GE',
-				'B01DCHMIF2',
-				'B00MWB5I9S',
-				'B06WD5QC6H',
-				]
+	ASIN_List = ['B06Y27GL9S', #Dr. Fulbright Kegel Balls
+	'''
+		'B0089175GE', #Magic Wand Speed Controller Dial Kit
+		'B01DCHMIF2', #Magic Wand Free Travel Wand
+		'B00MWB5I9S', #All In One 4 Tip Shower Enema System
+		'B06WD5QC6H', #Sex Stool
+		'B008ASBIU0', #Lube Launcher
+		'B01BHAJ6MO', #CleanStream Enema Silicone Attachment Kit
+		'B07L9J9518', #Lube Launcher 2 Pack
+		'B07GSH8STW', #Strapless Strap On Black
+		'B07GSG6LHH', #Strapless Strap On Pink
+		'B0091PJDX0', #CleanStream Anal Lubricant
+		'B00EF8OL0S', #Lidocaine Lube 8oz.
+		'B00375LMSE', #Passion 2 oz.
+		'B00375LMRU', #Passion 4 oz.
+		'B00711X53U', #Passion 8 oz.
+		'B004OEBMAK', #Passion 16 oz.
+		'B006JR1N72', #Passion 34 oz.
+		'B005MR3IVO', #Passion 55 gal. Water Based Drum
+		#'B07MKS1VZY', #Passion 55 gal. Performance Drum
+		'B07BH822QF', #Jesse Jane Doggy Style
+		'B07BH7TW7K', #Missionary Masturbator
+		'B07M5H7WFY', #Jesse Jane Bend Her Over
+		'B07M8WN6W5', #Jesse Jane Side Action
+	'''
+	]
 	extracted_data = []
 	for ASIN in ASIN_List:
 		url = "http://www.amazon.com/dp/"+ASIN
 		print ("Processing: " + url)
 		extracted_data.append(AmzonParser(url, ASIN))
 		sleep(5)
-	f=open('data.json','w')
+	file_name = time.strftime("%m-%d-%Y") + " Category Scrape.json"
+	print(file_name)
+	f=open(file_name,'w')
 	json.dump(extracted_data,f,indent=4)
 
+
+'''
+#def combineData :
+#	date
+	for f in glob.glob("/Users/justinm/Documents/Coding/Amazon"):
+			with open(f, "r", encoding="UTF-16") as read_file:
+
+'''
 
 if __name__ == "__main__":
 	ReadASIN()
