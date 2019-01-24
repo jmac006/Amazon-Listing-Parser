f8b5ba5db0406f02589f94fe74c86e51d78f6f6e
Justin Mac <jmac006@ucr.edu>
2019-01-24 14:22:17 -0800

    combined new and old data into graphs, cleaned up code

--- Amazon Scraper.py
+++ Amazon Scraper.py
@@ -23,13 +23,45 @@ import dash_core_components as dcc
 import dash_html_components as dashHTML
 import plotly.graph_objs as go
 import flask
-import pandas as pd
 
 #Global Variables
 ASIN_List = ['B06Y27GL9S', #Dr. Fulbright Kegel Balls
+			'B0089175GE', #Magic Wand Speed Controller Dial Kit
+			'B01DCHMIF2', #Magic Wand Free Travel Wand
+			'B00MWB5I9S', #All In One 4 Tip Shower Enema System
+			'B06WD5QC6H', #Sex Stool
+			'B008ASBIU0', #Lube Launcher
+			'B07L9J9518', #Lube Launcher 2 Pack
+			'B01BHAJ6MO', #CleanStream Enema Silicone Attachment Kit
+			'B07GSH8STW', #Strapless Strap On Black
+			'B07GSG6LHH', #Strapless Strap On Pink
+			'B01GIPO9B6', #Strapless Strap On Purple
+			'B0091PJDX0', #CleanStream Anal Lubricant
+			'B00EF8OL0S', #Lidocaine Lube 8oz.
+			'B00375LMSE', #Passion 2 oz.
+			'B00375LMRU', #Passion 4 oz.
+			'B00711X53U', #Passion 8 oz.
+			'B004OEBMAK', #Passion 16 oz.
+			'B006JR1N72', #Passion 34 oz.
+			'B005MR3IVO', #Passion 55 gal. Water Based Drum
+			'B07BH822QF', #Jesse Jane Doggy Style
+			'B07BH7TW7K', #Missionary Masturbator
+			'B07M5H7WFY', #Jesse Jane Bend Her Over
+			'B07M8WN6W5', #Jesse Jane Side Action
+			'B07GT67ZZK', #Magic Wand Wireless Controller
+			'B07B4NT8FY', #Thunderstick 2.0
+			'B00E5PZHOU', #Thunderstick Original
+			'B00W89XHQ0', #Black Dual Head Teaser
+			'B07C71LBG3', #Dual Vibrating Open Sheath
+			'B07HJW6XZ4', #Long Dual Head Teaser
+			'B07C6KBR6N', #Medium Dual Head Teaser
+			'B07HJRX2FN', #Short Dual Head Teaser
+			'B0037A6A1I', #Passion Silicone 2oz.
+			'B0071N16KC', #Passion Silicone 4oz.
+			'B0071N16Q6', #Passion Silicone 8oz.
 			]
 ASIN_Dict = [] #contains the name along with the ASIN
-extracted_data = []
+json_data = [] #list of dictionaries
 graph_data = [] #only update graph_data with specific ASIN when selected, instead of populating entire list of ASINs
 Products = []
 #optionList = []
@@ -37,7 +69,6 @@ optionList = [{'label': Products[index]["NAME"], 'value': Products[index]["ASIN"
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
 
-dataFrame = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
 
 app = dash.Dash('app', server=server)
 app.scripts.config.serve_locally = False
@@ -67,9 +98,9 @@ def update_dropdown(selected_dropdown_value):
 def update_graph(selected_dropdown_value):
 	#clear graph_data when called
 	#print("Selected value: ", selected_dropdown_value)
-	#call combineData(ASIN)
+	#call combineASINData(ASIN)
 
-	combineData(selected_dropdown_value)
+	combineASINData(selected_dropdown_value)
 
 	#print(DatabyCategory)
 	Date_range = [graph_data[index]["Date"] for index in range(len(graph_data))]
@@ -93,6 +124,8 @@ def update_graph(selected_dropdown_value):
 
 	dayRank = []
 	RecentDataIndex = len(graph_data)-1
+	#dynamically create the lines on the graph
+	#print(graph_data)
 	for i in range(len(graph_data[RecentDataIndex]["Ranks"])): #check to see how many rankings there are and iterate through the number of rankings
 		for day in range(len(Date_range)): #iterate through the days, find the Rankings, and check to see if there are no rankings for that day (y-coord)
 			if graph_data[day]["Ranks"] == []:
@@ -102,11 +135,12 @@ def update_graph(selected_dropdown_value):
 			dayRank.append(yRank)
 
 		categoryName = ' '.join(graph_data[day]["Ranks"][i].split()[2:])
-		print(categoryName)
+		#print(categoryName)
 		CategoryData.append({'x': Date_range, 
 			'y': dayRank,
 			'type': 'scatter',
 			'name': categoryName,
+			'showexponent': 'none',
 			'line': {
 			'width': 3,
 			'shape': 'spline'
@@ -116,6 +150,8 @@ def update_graph(selected_dropdown_value):
 	return{
 		'data': CategoryData,
 		'layout': {
+			'showlegend': True,
+			'showexponent': 'none',
 			'margin': {
 				'l': 30,
 				'r': 20,
@@ -125,7 +161,7 @@ def update_graph(selected_dropdown_value):
 		}
 	}
 
-def AmazonParser(url, ASIN):
+def AmazonParser(url, ASIN): #Grab information from ASIN URL and export it as a dictionary
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 	page = requests.get(url,headers=headers)
 	while True:
@@ -177,7 +213,7 @@ def AmazonParser(url, ASIN):
 				ORIGINAL_PRICE = SALE_PRICE
 
 			if page.status_code!=200:
-				print("Captcha")
+				print("Captcha Error")
 				raise ValueError('captha')
 				return data
 
@@ -212,22 +248,29 @@ def AmazonParser(url, ASIN):
 			print(e)
 			return data
 
-def ReadASIN():
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = '/' + path + '/' + fileName
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp,indent=4)
+
+def ReadASINList():
 	# ASIN_List = csv.DictReader(open(os.path.join(os.path.dirname(__file__),"Asinfeed.csv")))
 	
 	for ASIN in ASIN_List:
 		url = "http://www.amazon.com/dp/"+ASIN
 		print ("Processing: " + url)
-		extracted_data.append(AmazonParser(url, ASIN))
+		json_data.append(AmazonParser(url, ASIN))
 		sleep(5)
 	file_name = time.strftime("%m-%d-%Y") + " Category Scrape.json"
-	print(file_name)
-	f=open(file_name,'w')
-	json.dump(extracted_data,f,indent=4) #Convert dictionary to JSON file
+	#print(file_name)
+	#f=open(file_name,'w')	
+	#json.dump(json_data,f,indent=4) #Convert dictionary to JSON file
+	filePathName = "Users/justinm/Documents/Coding/Rankings"
+	writeToJSONFile(filePathName,file_name,json_data)
 
 
 
-def combineData(ASIN): #find all data for specific ASIN, while populating optionList
+def combineASINData(ASIN): #find all data for specific ASIN, while populating optionList
 #	date
 	path = glob.glob("/Users/justinm/Documents/Coding/Rankings/*.json") #Grab all JSON files from path
 	#file_names = [os.path.basename(x) for x in glob.glob('/Users/justinm/Documents/Coding/Rankings/*.json')]
@@ -247,7 +290,10 @@ def combineData(ASIN): #find all data for specific ASIN, while populating option
 				rankings = i["RANK"]
 				del rankings[0] #remove "Amazon Best Sellers Rank title from list"
 				file_date = os.path.basename(file).split()[0]
-				graph_data.append({'Date': file_date,'Ranks': rankings})
+				if file_date == "Baseline": #Baseline data
+					graph_data.insert(0,{'Date': i["DATE"],'Ranks': rankings})
+				else:
+					graph_data.append({'Date': file_date,'Ranks': rankings})
 	
 	#Products= list(set(Products)) #remove duplicate names			
 	#dates = [date.split()[0] for date in file_names] #split file name to list of dates
@@ -257,8 +303,10 @@ def combineData(ASIN): #find all data for specific ASIN, while populating option
 	#print(optionList)
 
 if __name__ == "__main__":
-	#print("Gathering new information on ASINs")
-	#ReadASIN() #Read new Data
-	combineData("Init")
+	grabInfo = input("Do you want to gather new information from ASIN List? (y/n)\n")
+	if grabInfo == 'y':
+		print("Gathering new information on ASINs")
+		ReadASINList() #Read New Data
+	combineASINData("Init")
 	#print(graph_data)
 	app.run_server() #Build Graph using Dash
